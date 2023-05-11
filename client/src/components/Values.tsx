3eff40ea4f67c142771c0216cd86af01d5565ace
birehan <birehananteneh4@gmail.com>
2023-05-11 03:00:18 +0300

    change in folder structure

--- client/src/components/Values.tsx
+++ client/src/components/Values.tsx
@@ -1,9 +1,24 @@
+import { ArrowDropDown } from '@mui/icons-material'
 import React from 'react'
 
 const Values = () => {
   return (
     <div>
-      values
+      <div>
+        <h1>Our Values</h1>
+      </div>
+
+      <div>
+        <div className='flex'>
+          <div>
+            <h1>id</h1>
+            <h1>title</h1>
+            <button><ArrowDropDown/></button>
+          </div>
+          <p>desc</p>
+        </div>
+        <div></div>
+      </div>
     </div>
   )
 }
