f1bf65e41b652ba14a674e37bd23ed29929066b3
birehan <birehananteneh4@gmail.com>
2023-05-11 03:00:18 +0300

    change in folder structure

---
+++ client/pages/index.tsx
@@ -0,0 +1,24 @@
+import { Container } from "@mui/material";
+import Footer from "../src/components/Footer";
+import Header from "../src/components/header/Header";
+import Landing from "../src/components/Landing";
+import Partners from "../src/components/Partners";
+import Projects from "../src/components/Projects";
+import Statistics from "../src/components/Statistics";
+import ThematicAreas from "../src/components/ThematicAreas";
+import Values from "../src/components/Values";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Landing />
+      <Projects />
+      <Statistics />
+      <ThematicAreas />
+      <Values />
+      <Partners />
+      <Footer />
+    </div>
+  );
+}
