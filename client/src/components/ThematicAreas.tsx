42509e8b8d772e6c7d763ee34a76b88bbf6528d4
addis <addisumotora3@gmail.com>
2023-05-14 19:31:34 +0300

    feat: thematice areas done

--- client/src/components/ThematicAreas.tsx
+++ client/src/components/ThematicAreas.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
-import React from "react";
 import { Poppins } from "next/font/google";
-import thematic from "../common/images/thematic.png";
+import thematic from "../../common/images/thematic.png";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+
+import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
+import { thematics } from "../../common/data";
+import { useEffect, useRef, useState } from "react";
 const popin = Poppins({
   weight: ["600"],
   style: ["normal"],
@@ -9,6 +14,27 @@ const popin = Poppins({
 });
 
 const ThematicAreas = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const splideRef = useRef(null);
+
+  useEffect(() => {
+    if (splideRef.current) {
+      splideRef.current.go(slideIndex);
+    }
+  }, [slideIndex]);
+
+  const handlePreviousSlide = () => {
+    setSlideIndex((slideIndex) =>
+      slideIndex === 0 ? thematics.length - 1 : slideIndex - 1
+    );
+  };
+
+  const handleNextSlide = () => {
+    setSlideIndex((prevIndex) =>
+      prevIndex === thematics.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
   return (
     <div>
       <div
@@ -28,28 +54,63 @@ const ThematicAreas = () => {
           with regard to child-centered community based development approach.{" "}
         </p>
       </div>
-      <div className="relative" >
-        <div className="block md:flex max-w-[1100px] m-auto thematic-card my-6">
-          <div className="flex-1">
-            <Image className="w-full h-full" src={thematic} alt="" />
-          </div>
-          <div className="flex-1 md:px-0 py-8 pr-4 mx-6 mr-3">
-            <h1 className="text-[#00B1F4] text-2xl not-italic ">Education</h1>
-            <p className="my-4 font-poppins  text-base leading-[2.23] tracking-[0.03em] text-[rgba(1, 1, 1, 0.74)]">
-              HFC is committed to providing comprehensive support for education,
-              from kindergarten to university level. Their support includes
-              school materials, uniforms, school fees, and tutorials. By
-              providing these resources, HFC ensures that children have the
-              tools they need to succeed in school.
-            </p>
-            <p className="font-poppins text-base leading-[2.23] trackzsing-[0.03em] text-[rgba(1, 1, 1, 0.74)]">
-              Thanks to HFC, more children are able to attend school and achieve
-              their dreams.
-            </p>
-            <button className="text-[#00B1F4] border border-[#00B1F4] rounded-[20px] p-2 w-[10rem] mt-5">
-              Read More
-            </button>
-          </div>
+      <div className="relative p-5">
+        <div className="corners max-w-[1150px] m-auto">
+          <div className="top left"></div>
+          <div className="top right"></div>
+          <div className="bottom right"></div>
+          <div className="bottom left"></div>
+          <Splide
+            options={{
+              rewind: true,
+              arrows: false,
+              pagination:false
+            }}
+            aria-label="React Splide Example"
+            ref={splideRef}
+            onMove={(splide) => {
+              setSlideIndex(splide.index);
+            }}
+          >
+            {thematics.map((them) => (
+              <SplideSlide>
+                <div className="block md:flex max-w-[1100px] m-auto thematic-card my-6">
+                  <div className="flex-1">
+                    <Image className="w-full h-full" src={thematic} alt="" />
+                  </div>
+                  <div className="flex-1 md:px-0 py-8 pr-4 mx-6 mr-3">
+                    <h1 className="text-[#00B1F4] text-3xl not-italic ">
+                      {them.title}
+                    </h1>
+                    <p className="my-4 font-poppins  text-base leading-[2.23] tracking-[0.03em] text-[rgba(1, 1, 1, 0.74)]">
+                      {them.desc}
+                    </p>
+                    <p className="font-poppins text-base leading-[2.23] trackzsing-[0.03em] text-[rgba(1, 1, 1, 0.74)]">
+                      {them.desc2}
+                    </p>
+                    <button className="text-[#00B1F4] border border-[#00B1F4] rounded-[20px] p-2 w-[10rem] mt-5">
+                      Read More
+                    </button>
+                  </div>
+                </div>
+              </SplideSlide>
+            ))}
+          </Splide>
+        </div>
+        <div className="flex justify-center items-center mt-2 gap-2">
+          <button
+            className="border rounded-full bg-gradient-to-r from-slate-300 via-transparent to-slate-300 bg-clip-padding backdrop-filter backdrop-blur-xl shadow-lg p-3"
+            onClick={handlePreviousSlide}
+          >
+            <ArrowBackIos className="text-[#00B1F4]" />
+          </button>
+
+          <button
+            className="border rounded-full bg-gradient-to-r from-slate-300 via-transparent to-slate-300 bg-clip-padding backdrop-filter backdrop-blur-xl shadow-lg p-3 text-[#00B1F4]"
+            onClick={handleNextSlide}
+          >
+            <ArrowForwardIos />
+          </button>
         </div>
       </div>
     </div>
---
+++ client/src/models/thematicAreas.ts
@@ -0,0 +1,8 @@
+export interface Thematic{
+    id: number,
+    title: string,
+    desc: string,
+    desc2?: string
+    link: string,
+    imageUrl: string
+}
