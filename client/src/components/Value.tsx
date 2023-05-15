b66e1a8d262a1db9d2e3296af7c4ffa2ac68a6b9
addis <addisumotora3@gmail.com>
2023-05-15 23:18:54 +0300

    Our values done

--- client/src/components/Value.tsx
+++ client/src/components/Value.tsx
@@ -1,26 +1,45 @@
 import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
+import { useState } from "react";
 import { values } from "../../common/data";
 
 const Value = () => {
+  const [selectedValue, setSelectedValue] = useState(null);
 
-  const toggleValue = () => {
-    
-  }
+  const toggleValue = (valueId: any | null) => {
+    setSelectedValue(prev => (prev === valueId ? null : valueId));
+  };
+
+  const getButtonRotation = (valueId: any | null) => {
+    return selectedValue === valueId ? "rotate-180" : "rotate-0";
+  };
+
   return (
     <div className="flex-1">
       {values.map((val) => (
-        <div className="flex flex-col items-center justify-center bg-white p-[24px] mb-6">
+        <div
+          key={val.id}
+          className="flex flex-col items-center justify-center bg-white p-[24px] mb-6"
+        >
           <div className="flex justify-between w-[100%]">
             <div className="flex justify-between items-center text-[24px]">
               <h1 className="text-[#00B1F473]">{val.id}</h1>
               <h1 className="ml-10 text-[#00B1F4]">{val.title}</h1>
             </div>
-            <button className="h-[40px] w-[40px] bg-gradient-to-r from-[rgba(216,218,229,0.24)] via-[rgba(215,216,219,0.06)] to-transparent shadow-xl backdrop-blur-[50px] rounded-[50%]">
+            <button
+              onClick={() => toggleValue(val.id)}
+              className={`h-[40px] w-[40px] bg-gradient-to-r from-[rgba(216,218,229,0.24)] via-[rgba(215,216,219,0.06)] to-transparent shadow-xl backdrop-blur-[50px] rounded-[50%] transform ${getButtonRotation(
+                val.id
+              )}`}
+            >
               <KeyboardArrowDown className="text-[#00B1F4]" />
             </button>
           </div>
-          <div>
-            {/* <p className='px-10 py-5'>{val.desc}</p> */}
+          <div
+            className={`transition-all duration-500 ${
+              selectedValue === val.id ? "max-h-[100px]" : "max-h-0 overflow-hidden"
+            }`}
+          >
+            <p className="px-10 py-5">{val.desc}</p>
           </div>
         </div>
       ))}
