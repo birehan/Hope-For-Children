dd910c48a8bef1e8870d916176df467a28341d11
Addisu Motora <88212776+addisumotora@users.noreply.github.com>
2023-05-28 14:53:20 +0300

    Revert "Our gallery"

--- client/src/components/header/Drawer.tsx
+++ client/src/components/header/Drawer.tsx
@@ -1,7 +1,8 @@
-import { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import {
   Drawer,
   List,
+  Button,
   Stack,
   Typography,
   ListItem,
@@ -9,10 +10,32 @@ import {
   Divider,
 } from "@mui/material";
 import { Box } from "@mui/system";
+// import { useSelector } from "react-redux";
 import CloseIcon from "@mui/icons-material/Close";
+import Logout from "@mui/icons-material/Logout";
+
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
+import Logo from "../assets/logo.png";
 import links from "./HeaderLinks";
 import AchievementDropDown from "./AchievementDropDown";
-import useStyles from "./styles";
+
+const useStyles = () => {
+  return {
+    drawerSite: {
+      color: "black",
+      fontSize: { lg: "27px", md: "22px" },
+      textDecoration: "none",
+      p: "5px 0",
+      "&:hover": {
+        cursor: "pointer",
+        color: "#4d99b6",
+        transition: "300ms all ease-in",
+      },
+      width: "100%",
+    },
+  };
+};
 
 interface Props {
   openDrawer: boolean;
@@ -30,40 +53,99 @@ const DrawerComponent = ({ openDrawer, setOpenDrawer }: Props) => {
         onClose={() => setOpenDrawer(false)}
       >
         <List>
-          <ListItem sx={classes.listItem}>
-            <Box sx={classes.drawerSite}>
-              <Stack sx={classes.drawerContent}>
-                <Link href="/" sx={classes.drawerLink}>
-                  <Box
-                    component="img"
-                    alt="logo"
-                    src="/assets/images/logo.jpg"
-                    sx={classes.logo}
-                  ></Box>
+          <ListItem sx={{ flexDirection: "column" }}>
+            <Link sx={classes.drawerSite}>
+              <Stack
+                sx={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Box>
+                  {" "}
+                  <Link
+                    onClick={() => {
+                      setOpenDrawer(false);
+                    }}
+                    sx={{
+                      textDecoration: "none",
+                      color: "#078989",
+                      fontSize: "24px",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    <Stack
+                      sx={{
+                        flexDirection: "row",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        gap: "10px",
+                      }}
+                    >
+                      <Box
+                        component="img"
+                        alt="logo"
+                        src="/assets/images/logo.jpg"
+                        sx={{
+                          width: "40px",
+                          height: "40px",
+                        }}
+                      ></Box>
 
-                  <Typography sx={classes.title}>Hope For Children</Typography>
-                </Link>
+                      <Typography
+                        sx={{
+                          fontSize: "22px",
+                          color: "#00b1f4",
+                        }}
+                      >
+                        Hope For Children
+                      </Typography>
+                    </Stack>
+                  </Link>
+                </Box>
 
                 <CloseIcon onClick={() => setOpenDrawer(false)} />
               </Stack>
-            </Box>
+            </Link>
           </ListItem>
-          <Divider />
           {links.map((link, index) => {
             if (link.name === "Achievements") {
               return <AchievementDropDown IsSmallScreen={true} key={index} />;
             }
-
             return (
               <Stack key={index}>
                 <ListItem>
-                  <Link href={link.path} sx={classes.link}>
-                    <Typography sx={{ fontSize: "1.2rem", fontWeight: "400" }}>
+                  <Link
+                    onClick={() => {
+                      setOpenDrawer(false);
+                    }}
+                    sx={{
+                      color: "black",
+                      fontSize: { lg: "27px", md: "22px" },
+                      textDecoration: "none",
+                      p: "5px 0",
+
+                      "&:hover": {
+                        cursor: "pointer",
+                        color: "#4d99b6",
+                        transition: "300ms all ease-in",
+                      },
+                      width: "100%",
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        // color: "white",
+                        fontSize: "1.2rem",
+                        fontWeight: "400",
+                      }}
+                    >
                       {link.name}
                     </Typography>
                   </Link>
                 </ListItem>
-                <Divider sx={classes.divider} />
+                <Divider />
               </Stack>
             );
           })}
