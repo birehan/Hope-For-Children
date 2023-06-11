4e59f57a3be73e6e8e597c9c98c52cf2e4bf291b
addis <addisumotora3@gmail.com>
2023-06-11 09:27:50 +0300

    feat: alumni page is done

---
+++ client/pages/alumni.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AlumniHero from '../src/components/Alumni/AlumniHero'
+import StudentList from '../src/components/Alumni/StudentList'
+
+const alumni = () => {
+  return (
+    <div>
+        <AlumniHero/>
+        <StudentList/>
+    </div>
+  )
+}
+
+export default alumni
