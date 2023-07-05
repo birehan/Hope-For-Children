4d5ea0e4c7787977bb4dd0de6155d16d4035024d
addisumotora <addisumotora3@gmail.com>
2023-07-04 21:47:56 -0500

    feat: contact page

---
+++ client/pages/contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Contact from '../src/components/Contact'
+
+const contanct = () => {
+  return (
+    <div>
+      <Contact/>
+    </div>
+  )
+}
+
+export default contanct
