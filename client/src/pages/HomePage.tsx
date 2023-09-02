05c9155917082415c04c3db139c062085b99ce6a
birehan <birehananteneh4@gmail.com>
2023-09-02 12:10:18 +0300

    fix

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -20,7 +20,7 @@ const HomePage: React.FC = () => {
   return (
     <div className="flex flex-col gap-16">
       <Landing />
-      <AboutComponent />
+      {/* <AboutComponent /> */}
       <ProjectsComponent />
       <Statistics />
       <ThematicAreas />
