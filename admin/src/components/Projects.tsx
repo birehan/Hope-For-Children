5a1e133d96f42fbade0aaeaef70066788cde6207
birehan <birehananteneh4@gmail.com>
2023-09-08 00:10:35 +0300

    project update

--- admin/src/components/Projects.tsx
+++ admin/src/components/Projects.tsx
@@ -1,7 +1,7 @@
 const Projects = () => {
   return (
     <main className="py-10 ">
-      <div className="px-4 sm:px-6 lg:px-8">Projects changed</div>
+      <div className="px-4 sm:px-6 lg:px-8">Projects changes</div>
     </main>
   );
 };
